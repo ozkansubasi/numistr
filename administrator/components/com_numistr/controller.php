2fffd6862df5d20505a94943de098a099963b8d5
ozkansubasi <osubasig@gmail.com>
2025-09-07 17:08:58 +0300

    Create controller.php

---
+++ administrator/components/com_numistr/controller.php
@@ -0,0 +1,9 @@
+<?php
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+class NumistrAdminController extends BaseController
+{
+    protected $default_view = 'dashboard';
+}
