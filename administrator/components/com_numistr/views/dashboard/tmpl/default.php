cc5289d76bc61eefb9171c6e5674c78bb83c77f0
ozkansubasi <osubasig@gmail.com>
2025-09-07 17:11:27 +0300

    Create default.php

---
+++ administrator/components/com_numistr/views/dashboard/tmpl/default.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <h2>Numistr Bileşeni</h2>
+    <p>Bu yönetim ekranı minimaldir. Ayarlar için sağ üstteki <strong>Seçenekler (Options)</strong> butonunu kullanın.</p>
+    <p><em>İpucu:</em> Bölge eşleştirme tablosunu JSON olarak Seçenekler'den girin. Örnek:<br>
+    <code>{ "galatya-sikkeleri": "galatia", "kappadokia-sikkeleri": "cappadocia" }</code></p>
+</div>
